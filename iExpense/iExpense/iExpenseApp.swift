31f38e1ec4dd97f82c44e392b76fe2dad8f3e939
Akash Sheelavant <akashsheelavant4@gmail.com>
2023-12-18 22:26:02 +0530

    Initial project.

---
+++ iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Akash Sheelavant on 12/18/23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
