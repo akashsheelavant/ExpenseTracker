50aa4542014d5dd0aebb49f593e1fff447a4b34b
Akash Sheelavant <akashsheelavant4@gmail.com>
2023-12-24 18:40:24 +0530

    Showing and hiding views.

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -13,17 +13,30 @@ import Observation
     var lastName = "Baggins"
 }
 
+struct SecondView: View {
+    @Environment(\.dismiss) var dismiss
+    
+    var body: some View {
+        Button("Dismiss") {
+            dismiss()
+        }
+    }
+}
+
 struct ContentView: View {
     
     @State private var user = User()
     
+    @State private var showingSheet = false
+    
     var body: some View {
-        VStack {
-            Text("Your name is \(user.firstName) \(user.lastName)")
-            
-            TextField("First name", text: $user.firstName)
-            TextField("Last Name", text: $user.lastName)
+        Button("Show Sheet") {
+            //show the sheet
+            showingSheet.toggle()
         }
+        .sheet(isPresented: $showingSheet, content: {
+            SecondView()
+        })
     }
 }
 
