78ef0c344aa7f061d079400251a290e24386c42c
Akash Sheelavant <akashsheelavant4@gmail.com>
2024-01-01 16:50:35 +0530

    Dismissing AddView on adding expense item and displaying more data about expense item.

--- iExpense/iExpense/AddView.swift
+++ iExpense/iExpense/AddView.swift
@@ -12,6 +12,8 @@ struct AddView: View {
     @State private var type = "Personal"
     @State private var amount = 0.0
     
+    @Environment(\.dismiss) var dismiss
+    
     var expenses: Expenses
     let types = ["Business", "Personal"]
     
@@ -34,6 +36,7 @@ struct AddView: View {
                 Button("Save") {
                     let item = ExpenseItem(name: name, type: type, amount: amount)
                     expenses.items.append(item)
+                    dismiss()
                 }
             }
         }
